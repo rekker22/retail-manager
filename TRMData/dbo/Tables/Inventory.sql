22bed25024efd1f833ec944d9477778365ba925b
subhasish Mandal <sm8017797063@gmail.com>
2019-05-04 18:01:16 +0530

    Registering DB Tables

---
+++ TRMData/dbo/Tables/Inventory.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[Inventory]
+(
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [ProductId] NCHAR(10) NULL, 
+    [Quantity] NCHAR(10) NULL, 
+    [PurchasePrice] MONEY NULL, 
+    [PurchaseDate] NCHAR(10) NULL
+)
