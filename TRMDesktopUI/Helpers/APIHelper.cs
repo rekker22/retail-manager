cd21a3c8a63edb9c414de2e571e377db7edf8575
subhasish Mandal <sm8017797063@gmail.com>
2019-05-13 02:30:28 +0530

    UI and API Wiring
    Created UI and WIred up Login API for Token

---
+++ TRMDesktopUI/Helpers/APIHelper.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using TRMDesktopUI.Models;
+
+namespace TRMDesktopUI.Helpers
+{
+    public class APIHelper : IAPIHelper
+    {
+        public HttpClient _apiClient { get; set; }
+
+        public APIHelper()
+        {
+            InitializeClient();
+        }
+
+        private void InitializeClient()
+        {
+            _apiClient = new HttpClient();
+            string api = ConfigurationManager.AppSettings["api"];
+            _apiClient.BaseAddress = new Uri(api);
+            _apiClient.DefaultRequestHeaders.Accept.Clear();
+            _apiClient.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
+        }
+
+        public async Task<AutheticatedUser> AuthenticateAsync(string username,string password)
+        {
+            var data = new FormUrlEncodedContent(new[]
+            {
+                new KeyValuePair<string,string>("grant_type","password"),
+                new KeyValuePair<string,string>("username",username),
+                new KeyValuePair<string,string>("password",password)
+            });
+
+            using (HttpResponseMessage response = await _apiClient.PostAsync("/token", data))
+            {
+                if (response.IsSuccessStatusCode)
+                {
+                    var result = await response.Content.ReadAsAsync<AutheticatedUser>();
+                    return result;
+                }
+                else
+                {
+                    throw new Exception(response.ReasonPhrase);
+                }
+            }
+        }
+    }
+}
